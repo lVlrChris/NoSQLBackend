5702e8113363d27d247d97409014152ae7dd3e2e
Chris <christian-boer@hotmail.com>
2019-04-08 09:36:34 +0200

    environment variables

--- startup/config.js
+++ startup/config.js
@@ -2,6 +2,12 @@ const config = require("config")
 
 // Checks config variables
 module.exports = function() {
+    //Set optional env variables
+    if (process.env.dbUrl) config.dbUrl = process.env.dbUrl;
+    if (process.env.neo4jBolt) config.neo4jBolt = process.env.neo4jBolt;
+    if (process.env.neo4jUsername) config.neo4jUsername = process.env.neo4jUsername;
+    if (process.env.neo4jPassword) config.neo4jPassword = process.env.neo4jPassword;
+
     if (!config.get("dbUrl")) {
         throw new Error('ERROR: dbUrl is not defined')
     }
